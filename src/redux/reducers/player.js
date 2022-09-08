25ec766d737fc7547436b6723e40e62146aedded
Everton Pires dos Santos <evertonpsantos8@gmail.com>
2022-09-08 15:18:10 -0300

    Configurando redux

---
+++ src/redux/reducers/player.js
@@ -0,0 +1,15 @@
+const INIT_STATE = {
+  name: '',
+  assertions: '',
+  score: '',
+  gravatarEmail: '',
+};
+
+const player = (state = INIT_STATE, action) => {
+  switch (action.type) {
+  default:
+    return state;
+  }
+};
+
+export default player;
